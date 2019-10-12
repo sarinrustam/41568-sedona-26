79684f2e654f6fcb14caebfde552816ba73e2199
Rustam Sarin <lorienas@gmail.com>
2019-10-13 00:58:30 +0300

    Added js and iframe

---
+++ js/script.js
@@ -0,0 +1,46 @@
+let buttonSearch = document.querySelector('.searching__button');
+let popup = document.querySelector('.searching-form');
+let loginArrival = popup.querySelector('[name=date-arrival]');
+let loginDeparture = popup.querySelector('[name=date-departure]');
+let people = popup.querySelector('.count__input')
+let form = popup.querySelector('form');
+let storage = "";
+let isStorageSupport = true;
+
+try {
+    storage = localStorage.getItem('loginArrival');
+} catch {
+    isStorageSupport = false;
+}
+
+
+popup.classList.add('searching-form--hide');
+
+buttonSearch.addEventListener('click', function(evt) {
+    evt.preventDefault();
+    if (popup.classList.contains('searching-form--error')) {
+        popup.classList.remove('searching-form--error');
+    }
+    popup.classList.toggle('searching-form--hide');
+    if (storage) {
+        loginArrival.value = storage;
+        loginDeparture.focus();
+    } else {
+        loginArrival.focus();
+    }
+
+    loginArrival.focus();
+});
+
+form.addEventListener('submit', function(evt) {
+    if (!loginArrival.value || !loginDeparture.value || !people.value) {
+        evt.preventDefault();
+        popup.classList.remove('searching-form--error');
+        popup.offsetWidth = popup.offsetWidth;
+        popup.classList.add('searching-form--error');
+    } else {
+        if (isStorageSupport) {
+            localStorage.setItem('loginArrival', loginArrival.value);
+        }
+    }
+})
